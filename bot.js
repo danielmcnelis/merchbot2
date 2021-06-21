c6e0e90ba581dbd50cd2403927e71a18197b99f4
Daniel McNelis <dwm253@gmail.com>
2021-06-21 16:57:47 -0400

    id hack

--- bot.js
+++ bot.js
@@ -163,7 +163,7 @@ if (cmd === `!update`) {
 	})
 
 	let created = 0
-	let card_id = 9712
+	let cards_id = 9712
 
 	for (let i = 0; i < newCards.length; i++) {
 		const newCard = newCards[i]
@@ -185,7 +185,7 @@ if (cmd === `!update`) {
 		
 		try {
 			await Card.create({
-				card_id,
+				cards_id,
 				image,
 				name,
 				card,
@@ -205,7 +205,7 @@ if (cmd === `!update`) {
 		}
 
 		created++
-		card_id++
+		cards_id++
 	}	
 
 	console.log('created', created)

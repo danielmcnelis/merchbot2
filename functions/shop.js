4332b7c0248ebb781107a5773858f2a3f7e88cdb
Daniel McNelis <dwm253@gmail.com>
2021-06-26 21:18:08 -0400

    timezone tricks

--- functions/shop.js
+++ functions/shop.js
@@ -80,24 +80,23 @@ const getShopCountdown = () => {
 	const minsLeftInPeriod = 60 - mins
 
     if ((day === 6 && hours >= 18) || day === 0 || day === 1 || (day === 2 && hours < 20)) {
-        hoursLeftInPeriod = day === 6 ? 24 - hours + 24 * 2 + 15 :
-			day === 0 ? 24 - hours + 24 + 15 :
-			day === 1 ? 24 - hours + 15 :
-			day === 2 ? 15 - hours :
+        hoursLeftInPeriod = day === 6 ? 23 - hours + 24 * 2 + 20 :
+			day === 0 ? 23 - hours + 24 + 20 :
+			day === 1 ? 23 - hours + 20 :
+			day === 2 ? 19 - hours :
 			null
     } else if ((day === 2 && hours >= 20) || (day === 3 && hours < 12)) {
         hoursLeftInPeriod = day === 2 ? 24 - hours + 8 :
-        day === 3 ? 7 - hours :
+        day === 3 ? 11 - hours :
         null
     } else if ((day === 3 && hours >= 12) || day === 4 || day === 5 || (day === 6 && hours < 2)) {
-        hoursLeftInPeriod = day === 3 ? 24 - hours + 24 + 22 :
-			day === 4 ? 24 - hours + 21 :
-			day === 5 ? 21 - hours :
+        hoursLeftInPeriod = day === 3 ? 23 - hours + 24 * 2 + 2 :
+			day === 4 ? 23 - hours + 24 + 2 :
+			day === 5 ? 23 - hours + 2 :
+			day === 6 ? 1 - hours :
 			null
     } else if ((day === 6 && hours >= 2 && hours < 18)) {
-        hoursLeftInPeriod = day === 5 ? 24 - hours + 13 :
-			day === 6 ? 13 - hours :
-			null
+        hoursLeftInPeriod = 17 - hours
     }
 
     console.log('hoursLeftInPeriod', hoursLeftInPeriod)

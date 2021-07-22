d8c2e272a2782940ae32a360e55dc7bd7d16490f
Daniel McNelis <dwm253@gmail.com>
2021-07-21 20:34:15 -0400

    trade history

--- bot.js
+++ bot.js
@@ -1732,14 +1732,14 @@ if(cmd === `!trades`) {
 
 	for (let i = 0; i < summaries.length; i++) {
 		const summary = summaries[i]
-		results.push(`Trade ${i+1}:\n${summary.p1_name}\n\nreceived:\n${summary.p1_receives.join("\n")}\n\n${summary.p2_name} received:\n${summary.p2_receives.join("\n")}`)
+		results.push(`Trade ${i+1}:\n${summary.p1_name} received:\n${summary.p1_receives.join("\n")}\n\n${summary.p2_name} received:\n${summary.p2_receives.join("\n")}`)
 	}
 
 	players.sort()
 	//message.channel.send(`You have traded with the following players:\n${players.join("\n")}`)
-	message.channel.send(`${results.slice(0, 5).join("\n----------------")}`)
+	message.channel.send(`${results.slice(0, 5).join("\n----------------\n")}`)
 	for (let i = 5 ; i < results.length; i += 5) {
-		message.channel.send(results.slice(i, i + 5).join("\n----------------"))
+		message.channel.send(results.slice(i, i + 5).join("\n----------------\n") + "\n----------------")
 	}
 	return
 }

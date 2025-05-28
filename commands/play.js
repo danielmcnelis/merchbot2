8b70700779a0d4af9f738ec5a2d6e5e8fa7f5c5f
Daniel McNelis <dwm253@gmail.com>
2025-05-27 23:05:00 -0400

    merchbot 3.0

--- commands/play.js
+++ commands/play.js
@@ -10,7 +10,7 @@ export default {
 		.setDescription('Play the game! 🎮'),
 	async execute(interaction) {
         try {
-            // await interaction.deferReply()
+            await interaction.deferReply()
             const player = await Player.findByDiscordId(interaction.member.user.id)
             const walletExists = await Wallet.count({ where: { playerId: player.id }})
             if (walletExists) return await interaction.reply({ content: `Error: You already began the game!` })
@@ -19,7 +19,7 @@ export default {
             const set = await ForgedSet.findOne({ where: { code: 'AOD' }})
             await awardPacks(interaction, interaction.member, set, 8)
             await sendInventoryYDK(interaction, player)
-            return interaction.reply({ content: `${player.name} began the game! Please check your DMs for your first 8 packs, then try out the command **/inventory**!` })
+            return interaction.editReply({ content: `${player.name} began the game! Please check your DMs for your first 8 packs, then try out the command **/inventory**!` })
         } catch (err) {
             console.log(err)
         }

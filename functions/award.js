0d5a95f108033536728d109e3fb55cd3d738d5e3
Daniel McNelis <dwm253@gmail.com>
2025-06-10 15:18:53 -0400

    merchbot 3.0

--- functions/award.js
+++ functions/award.js
@@ -1,16 +1,17 @@
-const { Auction, Bid, Print, Set, Inventory, Player, Wallet } = require('../database/index.js')
-const { yescom, nocom } = require('../static/commands.json')
-const { findCard } = require('./search.js')
-const { completeTask } = require('./diary.js')
-const { DRT, fiend, thunder, zombie, beast, blue, bronze, cactus, cavebob, checkmark, com, credits, cultured, diamond, dinosaur, DOC, LPK, egg, emptybox, skull, familiar, battery, evil, FiC, fire, fish, god, gold, hook, koolaid, leatherbound, legend, lmfao, mad, master, merchant, milleye, moai, mushroom, no, ORF, TEB, FON, warrior, spellcaster, dragon, plant, platinum, rar, red, reptile, rock, rocks, rose, sad, scr, silver, soldier, starchips, stardust, stare, dimmadome, sup, tix, ult, wokefrog, yellow, yes, ygocard } = require('../static/emojis.json')
-const { Message } = require('discord.js')
+// const { Auction, Bid, Print, Set, Inventory, Player, Wallet } = require('../database/index.js')
+import { ForgedInventory, ForgedPrint } from '../database/index.js'
+// const { yescom, nocom } = require('../static/commands.json')
+// const { findCard } = require('./search.js')
+// const { completeTask } = require('./diary.js')
+// const { DRT, fiend, thunder, zombie, beast, blue, bronze, cactus, cavebob, checkmark, com, credits, cultured, diamond, dinosaur, DOC, LPK, egg, emptybox, skull, familiar, battery, evil, FiC, fire, fish, god, gold, hook, koolaid, leatherbound, legend, lmfao, mad, master, merchant, milleye, moai, mushroom, no, ORF, TEB, FON, warrior, spellcaster, dragon, plant, platinum, rar, red, reptile, rock, rocks, rose, sad, scr, silver, soldier, starchips, stardust, stare, dimmadome, sup, tix, ult, wokefrog, yellow, yes, ygocard } = require('../static/emojis.json')
+// const { Message } = require('discord.js')
 
 export const awardCard = async (channel, playerId, cardCode, quantity = 1) => {
-    const print = await Print.findOne({ where: { cardCode }})
+    const print = await ForgedPrint.findOne({ where: { cardCode }})
     if (!print) return channel.send({ content: `Could not find print: "${cardCode}".`})
     const card = `${eval(print.rarity)}${cardCode} - ${print.cardName}`
 
-    const inv = await Inventory.findOne({
+    const inv = await ForgedInventory.findOne({
         where: {
             playerId,
             cardCode
@@ -21,16 +22,16 @@ export const awardCard = async (channel, playerId, cardCode, quantity = 1) => {
         inv.quantity += quantity
         await inv.save()
     } else {
-        await Inventory.create({
+        await ForgedInventory.create({
             playerId,
             cardCode,
             quantity,
-            printId: print.id
+            forgedPrintId: print.id
         })
 
-        if (print.rarity === 'scr') completeTask(channel, playerId, 'm4')
+        // if (print.rarity === 'scr') completeTask(channel, playerId, 'm4')
     }
 
-    if (print.setCode === 'APC' && ((inv && inv.quantity >= 3) || quantity >=3 )) completeTask(channel, playerId, 'h5')
+    // if (print.setCode === 'APC' && ((inv && inv.quantity >= 3) || quantity >=3 )) completeTask(channel, playerId, 'h5')
     return channel.send({ content: `<@${playerId}> was awarded ${quantity} ${card}. Congratulations!`})
 }

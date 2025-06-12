817bc39e789ecf9fbbaafa0996e52d0aa8074fb8
Daniel McNelis <dwm253@gmail.com>
2025-06-12 11:35:09 -0400

    merchbot compatibility

--- commands/decks.js
+++ commands/decks.js
@@ -2,6 +2,7 @@
 import { SlashCommandBuilder } from 'discord.js'
 import arenas from '../static/arenas.json' with { type: 'json' }
 const { decks } = arenas 
+import { capitalize } from '../functions/utility'
 
 export default {
     data: new SlashCommandBuilder()
@@ -11,7 +12,7 @@ export default {
         try {
             if (interaction.channel.name !== 'arena') return await interaction.reply({ content: `Try using **/decks** in channels like: <#1378129840691220631>.`})
             console.log('decks', decks)
-                const deckUrls = Object.entries(decks).map((e) => `${e[0]} - <${e[1].url}>`)            
+                const deckUrls = Object.entries(decks).map((e) => `${capitalize(e[0])} - <${e[1].url}>`)            
             return await interaction.reply({ content: `The Arena Decks:\n` + deckUrls.join('\n').toString() })
         } catch (err) {
             console.log(err)

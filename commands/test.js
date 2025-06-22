7354dd9b72d00c1bbd45cd81588e3947641c4877
Daniel McNelis <dwm253@gmail.com>
2025-06-22 12:01:46 -0400

    forged table

--- commands/test.js
+++ commands/test.js
@@ -5,7 +5,7 @@ import { ActionRowBuilder, ButtonBuilder, ButtonStyle, InteractionContextType, S
 // import { client } from '../client'
 // import { s3FileExists } from '@fl/bot-functions'
 // import { Match, Tournament, Server, TriviaQuestion } from '@fl/models'
-import { ArenaProfile, Binder, ForgedInventory, Wishlist } from '../database/index.js'
+import { ArenaProfile, Binder, ForgedInventory, Wishlist, ForgedPrint, ForgedSet } from '../database/index.js'
 import { Op } from 'sequelize'
 import {isProgrammer} from '../functions/utility.js'
 import {applyPriceDecay} from '../functions/shop.js'

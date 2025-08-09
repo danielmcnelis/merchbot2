e84f4d0e504c61cfc4d4e8dcfbb7c71b78609403
Daniel McNelis <dwm253@gmail.com>
2025-08-08 22:02:26 -0400

    promos to auction

--- commands/test.js
+++ commands/test.js
@@ -10,6 +10,7 @@ import { Op } from 'sequelize'
 import {isProgrammer} from '../functions/utility.js'
 import {applyPriceDecay} from '../functions/shop.js'
 import {startRound, postStandings} from '../functions/arena.js'
+import {awaitAwardPromosToShop} from '../functions/packs.js'
 // import axios from 'axios'
 // import { assignTournamentRoles, recalculateAllStats } from '../../../bot-functions/src'
 // import { Stats } from '../../../models/src'
@@ -84,7 +85,7 @@ export default {
                 //     if (card) await inv.update({ cardId: card.id })
                 // }
 
-                await calcBoxPrice()
+                await awaitAwardPromosToShop()
 
                 // const entries = await ArenaEntry.findAll({ order: [['score', 'DESC']], include: Player })
 

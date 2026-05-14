6b8ad5a9030ebfbf596d09248f4b6f185f14526b
Daniel McNelis <dwm253@gmail.com>
2026-05-13 22:30:39 -0400

    test

--- commands/play.js
+++ commands/play.js
@@ -40,16 +40,16 @@ export default {
               
             const previousDate = new Date('2026-05-13')
             const days = daysSince(previousDate)
-            let starchips = 48 + (12 * days)
-            if (starchips > 400) starchips = 400
-            let stardust = 100 * days
+            let starchips = 10 + (10 * days)
+            if (starchips > 300) starchips = 300
+            let stardust = 100 + 100 * days
             if (stardust > 3000) stardust = 3000
             await Wallet.create({ playerId: player.id, playerName: player.name, starchips, stardust })
 
             const set = await ForgedSet.findOne({ where: { code: 'LDM' }})
-            // await interaction.editReply({ content: `${player.name} began the game! Please check your DMs for your first 12 packs and use the command **/inventory** to view your inventory!` })
-            await interaction.editReply({ content: `${player.name} began the game! Please check your DMs for your first 8 packs and a YDK file of your starting inventory!` })
-            await awardPacks(interaction.channel, interaction.member, set, 8)
+            await interaction.editReply({ content: `${player.name} began the game! Please check your DMs for your first 12 packs and use the command **/inventory** to view your inventory!` })
+            // await interaction.editReply({ content: `${player.name} began the game! Please check your DMs for your first 8 packs and a YDK file of your starting inventory!` })
+            await awardPacks(interaction.channel, interaction.member, set, 12)
             // await awardBox(interaction.channel, interaction.member, set, 24)
             return await sendInventoryYDK(interaction, player)
         } catch (err) {

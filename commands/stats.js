d1dde1a43850eaf9138fc59364e23218285c3267
Daniel McNelis <dwm253@gmail.com>
2026-05-17 17:42:34 -0400

    test

--- commands/stats.js
+++ commands/stats.js
@@ -3,14 +3,14 @@ import { InteractionContextType, SlashCommandBuilder } from 'discord.js'
 import { ForgedInventory, ForgedPrint, Player, Trade, Wallet } from '../database/index.js'
 import { Op } from 'sequelize'
 import emojis from '../static/emojis.json' with { type: 'json' }
-const { scheming, robbed, starchips, LDM } = emojis
+const { king, master, scheming, robbed, starchips, rock, gold, LDM } = emojis
 import channels from '../static/channels.json' with { type: 'json' }
 const { marketPlaceChannelId, arenaChannelId } = channels
 
 export default {
 	data: new SlashCommandBuilder()
-		.setName('stats')
-		.setDescription('View your net worth or Arena stats! 🪎')
+		.setName('networth')
+		.setDescription('View your net worth! 🪎')
         .addUserOption(option =>
             option
                 .setName('player')
@@ -20,84 +20,86 @@ export default {
     	.setContexts(InteractionContextType.Guild),
         async execute(interaction) {
         try {
-            console.log('?')
             await interaction.deferReply()
             const user = interaction.options.getUser('player') || interaction.user
             const player = await Player.findOne({ where: { discordId: user?.id } })
 
-            if (interaction.channelId === marketPlaceChannelId) {
-                const wallet = await Wallet.findOne({ where: { playerId: player.id }, include: Player})
-                if (!wallet && player.id === interaction.user.id) return await interaction.editReply({ content: `You are not in the database. Type **/play** to begin the game.`})
-                if (!wallet && player.id !== interaction.user.id) return await interaction.editReply({ content: `That user is not in the database.`})
-            
-                let networth = wallet.stardust * 0.1 + wallet.starchips
-                console.log('networth', networth)
-                let completeSets = []
-                let printCount = 0
-                let ldmCount = 0
+            const wallet = await Wallet.findOne({ where: { playerId: player.id }, include: Player})
+            if (!wallet && player.id === interaction.user.id) return await interaction.editReply({ content: `You are not in the database. Type **/play** to begin the game.`})
+            if (!wallet && player.id !== interaction.user.id) return await interaction.editReply({ content: `That user is not in the database.`})
+        
+            let networth = wallet.stardust * 0.1 + wallet.starchips
 
-                const printTotal = await ForgedPrint.count()
-                
-                const invs = await ForgedInventory.findAll({ where: { playerId: player.id }, include: [ForgedPrint] })
-                console.log('invs.length', invs.length)
+            let completeSets = []
+            let printCount = 0
+            let ldmCount = 0
+
+            const printTotal = await ForgedPrint.count()
+            
+            const invs = await ForgedInventory.findAll({ 
+                where: { 
+                    playerId: player.id 
+                }, 
+                include: ForgedPrint
+            })
+
+            for (let i = 0; i < invs.length; i++) {
+                const inv = invs[i]
+                networth += inv.quantity * inv.forgedPrint.marketPrice * 0.1
+                if (inv.quantity > 0) printCount++
+                if ((inv.cardCode.includes('LDM-0') || inv.cardCode.includes('LDM-1')) && inv.quantity >= 3) ldmCount++
+            }
+
+            const totalPlayers = await Wallet.count() - 1
+            const allNetworths = []
+
+            const wallets = await Wallet.findAll({
+                where: {
+                    playerId: {[Op.not]: 'ZXyLL1wTcEZXSZYtegEuTr'}
+                },
+                include: [Player]
+            })
+
+            for (let i = 0; i < wallets.length; i++) {
+                const wallet = wallets[i]
+                let networth = wallet.stardust * 0.1 + wallet.starchips
+                const invs = await ForgedInventory.findAll({ where: { playerId: wallet.player.id }, include: [ForgedPrint] })
                 for (let i = 0; i < invs.length; i++) {
                     const inv = invs[i]
                     networth += inv.quantity * inv.forgedPrint.marketPrice * 0.1
-                    if (inv.quantity > 0) printCount++
-                    if ((inv.cardCode.includes('LDM-0') || inv.cardCode.includes('LDM-1')) && inv.quantity >= 3) ldmCount++
                 }
 
-                const totalPlayers = await Wallet.count() - 1
-                const allNetworths = []
-
-                const wallets = await Wallet.findAll({
-                    where: {
-                        playerId: {[Op.not]: 'ZXyLL1wTcEZXSZYtegEuTr'}
-                    },
-                    include: [Player]
-                })
-
-                for (let i = 0; i < wallets.length; i++) {
-                    const wallet = wallets[i]
-                    let networth = wallet.stardust * 0.1 + wallet.starchips
-                    const invs = await ForgedInventory.findAll({ where: { playerId: wallet.player.id }, include: [ForgedPrint] })
-                    for (let i = 0; i < invs.length; i++) {
-                        const inv = invs[i]
-                        networth += inv.quantity * inv.forgedPrint.marketPrice * 0.1
-                    }
-
-                    console.log('networth i', networth, i)
-                    allNetworths.push(networth)
-                }
-
-                allNetworths.sort((a, b) => b - a)
-                console.log('allNetworths', allNetworths)
-                const networkRanking = allNetworths.indexOf(networth) + 1
-
-                console.log('ldmCount', ldmCount)
-                if (ldmCount === 200) completeSets.push(`Linked Dimensions ${LDM}`)
-
-                const tradeCount = await Trade.count({
-                    where: {
-                        [Op.or]: {
-                            recipientId: player.id,
-                            senderId: player.id
-                        }
-                    }
-                })
-
-                const results = [`${scheming} --- Marketplace Stats --- ${scheming}`]
-                results.push(`Name: ${player.name}`)
-                results.push(`Net Worth: ${Math.round(networth)} ${starchips}`)
-                results.push(`Ranking: ${networkRanking} out of ${totalPlayers}`)
-                results.push(`Prints: ${printCount} out of ${printTotal}`)
-                results.push(`Complete Sets: ${completeSets.length ? completeSets.join(',') : 'N/A'}`)
-                results.push(`Trades: ${tradeCount} ${robbed}`)
-            
-                return await interaction.editReply({ content: results.join('\n').toString()})
-            } else if (interaction.channelId === arenaChannelId) {
-                console.log('arena stats')
+                allNetworths.push(networth)
             }
+
+            allNetworths.sort((a, b) => b - a)
+            const networthRanking = allNetworths.indexOf(networth) + 1
+
+            if (ldmCount === 200) completeSets.push(`Linked Dimensions ${LDM}`)
+
+            const tradeCount = await Trade.count({
+                where: {
+                    [Op.or]: {
+                        recipientId: player.id,
+                        senderId: player.id
+                    }
+                }
+            })
+
+            const medal = networthRanking <= 0.1 * allNetworths.length ? king :
+                networthRanking > 0.1 * allNetworths.length && networthRanking <= 0.2 * allNetworths.length ? master :
+                networthRanking > 0.2 * allNetworths.length && networthRanking <= 0.8 ? gold :
+                rock
+
+            const results = [`${scheming} --- Marketplace Stats --- ${scheming}`]
+            results.push(`Name: ${player.name}`)
+            results.push(`Net Worth: ${Math.round(networth)} ${starchips}`)
+            results.push(`Ranking: ${networthRanking} out of ${totalPlayers} ${medal}`)
+            results.push(`Prints: ${printCount} out of ${printTotal} :flower_playing_cards:`)
+            results.push(`Complete Sets: ${completeSets.length ? completeSets.join(',') : 'N/A'}`)
+            results.push(`Trades: ${tradeCount} ${robbed}`)
+        
+            return await interaction.editReply({ content: results.join('\n').toString()})
         } catch (err) {
             console.log(err)
         }

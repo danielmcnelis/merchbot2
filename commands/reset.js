3f96ab1f7304979b423322ae142227b734c7fd79
Daniel McNelis <dwm253@gmail.com>
2026-05-17 10:42:18 -0400

    test

--- commands/reset.js
+++ commands/reset.js
@@ -41,95 +41,65 @@ export default {
                     .setStyle(ButtonStyle.Primary)
                 )
 
-            const message = await interaction.channel.send({ content: `Are you sure you want to reset your progress and donate all your cards to The Shop? ${merchant}`, components: [row] })
+            const message = await interaction.channel.send({ content: `Are you ***ABSOLUTELY SURE*** you want to reset your progress and donate all your cards to The Shop? ${merchant}`, components: [row] })
 
             const filter = i => i.customId.startsWith(`Reset-${timestamp}`) && i.user.id === interaction.user.id
 
             try {
                 const confirmation = await interaction.channel.awaitMessageComponent({ filter, time: 30000 })
                 if (confirmation.customId.includes('Yes')) {
-                    await interaction.editReply({ content: '🤔' , components: []})
-                    const timestamp = new Date().getTime()
+                    const invs = await ForgedInventory.findAll({ where: { playerId: player.id }, order: [["cardCode", "ASC"]] })
 
-                    const row = new ActionRowBuilder()
-                        .addComponents(new ButtonBuilder()
-                            .setCustomId(`Reset-${timestamp}-Yes`)
-                            .setLabel('RESET')
-                            .setStyle(ButtonStyle.Danger)
-                        )
+                    for (let i = 0; i < invs.length; i++) {
+                        const inv = invs[i]
+                        const quantity = inv.quantity
+                        const forgedPrintId = inv.forgedPrintId
 
-                        .addComponents(new ButtonBuilder()
-                            .setCustomId(`Reset-${timestamp}-No`)
-                            .setLabel('CANCEL')
-                            .setStyle(ButtonStyle.Primary)
-                        )
-
-                    const message = await interaction.channel.send({ content: `Are you ***ABSOLUTELY SURE*** you want to reset your progress and donate all your cards to The Shop? ${merchant}`, components: [row] })
-
-                    const filter = i => i.customId.startsWith(`Reset-${timestamp}`) && i.user.id === interaction.user.id
-
-                    try {
-                        const confirmation = await interaction.channel.awaitMessageComponent({ filter, time: 30000 })
-                        if (confirmation.customId.includes('Yes')) {
-                            const invs = await ForgedInventory.findAll({ where: { playerId: player.id }, order: [["cardCode", "ASC"]] })
-
-                            for (let i = 0; i < invs.length; i++) {
-                                const inv = invs[i]
-                                const quantity = inv.quantity
-                                const forgedPrintId = inv.forgedPrintId
-
-                                const merchbotInv = await ForgedInventory.findOne({
-                                    where: {
-                                        forgedPrintId: forgedPrintId,
-                                        playerId: 'ZXyLL1wTcEZXSZYtegEuTr'
-                                    }
-                                })
-
-                                if (merchbotInv) {
-                                    const newQuantity = merchbotInv.quantity + quantity
-                                    await merchbotInv.update({ quantity: newQuantity })
-                                } else {
-                                    await ForgedInventory.create({
-                                        cardName: inv.cardName,
-                                        cardCode: inv.cardCode,
-                                        forgedPrintId: forgedPrintId,
-                                        quantity: quantity,
-                                        playerName: 'MerchBot',
-                                        playerId: 'ZXyLL1wTcEZXSZYtegEuTr'
-                                    })
-                                }
-
-                                console.log(`Donating ${quantity} ${inv.cardCode} to MerchBot.`)
-                                console.log(`Destroying ${player.name}'s ${inv.cardCode} Inventory.`)
-                                await inv.destroy()
+                        const merchbotInv = await ForgedInventory.findOne({
+                            where: {
+                                forgedPrintId: forgedPrintId,
+                                playerId: 'ZXyLL1wTcEZXSZYtegEuTr'
                             }
+                        })
 
-                            const binders = await Binder.findAll({ where: { playerId: player.id } })
-                            for (let i = 0; i < binders.length; i++) {
-                                const binder = binders[i]
-                                await binder.destroy()
-                            }
-
-                            const daily = await Daily.findOne({ where: { playerId: player.id } })
-                            if (daily) await daily.destroy()
-
-                            const wallet = await Wallet.findOne({ where: { playerId: player.id } })
-                            if (wallet) await wallet.destroy()
-
-                            const wishlists = await Wishlist.findAll({ where: { playerId: player.id } })
-                            for (let i = 0; i < wishlists.length; i++) {
-                                const wishlist = wishlists[i]
-                                await wishlist.destroy()
-                            }
-
-                            return interaction.editReply({ content: `Your account has been successfully reset. All your cards have been donated to The Shop. Type **/play** to begin the game again` })
+                        if (merchbotInv) {
+                            const newQuantity = merchbotInv.quantity + quantity
+                            await merchbotInv.update({ quantity: newQuantity })
                         } else {
-                            await message.edit({ content: `Not a problem. Your progress was not reset. 🎮`, components: [] })
+                            await ForgedInventory.create({
+                                cardName: inv.cardName,
+                                cardCode: inv.cardCode,
+                                forgedPrintId: forgedPrintId,
+                                quantity: quantity,
+                                playerName: 'MerchBot',
+                                playerId: 'ZXyLL1wTcEZXSZYtegEuTr'
+                            })
                         }
-                    } catch (err) {
-                        console.log(err)
-                        await message.edit({ content: `Sorry, time's up. Your progress was not reset. 🎮`, components: [] });
+
+                        console.log(`Donating ${quantity} ${inv.cardCode} to MerchBot.`)
+                        console.log(`Destroying ${player.name}'s ${inv.cardCode} Inventory.`)
+                        await inv.destroy()
                     }
+
+                    const binders = await Binder.findAll({ where: { playerId: player.id } })
+                    for (let i = 0; i < binders.length; i++) {
+                        const binder = binders[i]
+                        await binder.destroy()
+                    }
+
+                    const daily = await Daily.findOne({ where: { playerId: player.id } })
+                    if (daily) await daily.destroy()
+
+                    const wallet = await Wallet.findOne({ where: { playerId: player.id } })
+                    if (wallet) await wallet.destroy()
+
+                    const wishlists = await Wishlist.findAll({ where: { playerId: player.id } })
+                    for (let i = 0; i < wishlists.length; i++) {
+                        const wishlist = wishlists[i]
+                        await wishlist.destroy()
+                    }
+
+                    return interaction.editReply({ content: `Your account has been successfully reset. All your cards have been donated to The Shop. Type **/play** to begin the game again` })
                 } else {
                     await message.edit({ content: `Not a problem. Your progress was not reset. 🎮`, components: [] })
                 }
